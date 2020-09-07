55624eebc118d4f7b75cc09f072819658f442e23
Katie Scruggs <kscruggs@joinhandshake.com>
2020-09-07 16:49:48 -0600

    Simplify queries and add an example mutation

--- server.js
+++ server.js
@@ -1,59 +1,31 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
-const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt } = require("graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: "Root",
-    fields: {
-      hello: {
-        description: "Querying 'hello' will return 'world'",
-        type: GraphQLString,
-        resolve() {
-          return "world";
-        },
-      },
-      error: {
-        description: "This query just throws an error",
-        type: GraphQLString,
-        resolve() {
-          throw new Error("Item with ID 1 not found");
-        }
-      },
-      aThing: {
-        description: "Querying 'aThing' will return the thing",
-        type: new GraphQLObjectType({
-          name: "aThing",
-          fields: {
-            ID: { type: GraphQLInt },
-            Title: { type: GraphQLString },
-            Description: { type: GraphQLString }
-          }
-        }),
-        args: {
-          one: { type: GraphQLString} ,
-          two: { type: GraphQLString },
-          three: { type: GraphQLString }
-        },
-        resolve(_, args) {
-          console.log("args", args);
-          return {
-            ID: 1,
-            Title: "This is a title",
-            Description: "This is a description of the thing"
-          };
-        }
-      }
-    },
-  }),
-});
+const schema = buildSchema(`
+  type Query {
+    hello: String,
+    error: String,
+  }
+
+  type Mutation {
+    setMessage(message: String): String,
+  }
+`);
+
+const root = {
+  hello: () => "world",
+  error: () => { throw new Error("example error!") },
+  setMessage: ({ message }) => message,
+};
 
 app.use(
   "/graphql",
   graphqlHTTP({
     schema,
+    rootValue: root,
     graphiql: true
   })
 );
